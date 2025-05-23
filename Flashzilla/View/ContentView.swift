2b92f49c1a6467028aae1d6886884cd56a6ca5ca
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-23 16:30:55 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/counting-down-with-a-timer

--- Flashzilla/View/ContentView.swift
+++ Flashzilla/View/ContentView.swift
@@ -7,18 +7,28 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.accessibilityReduceTransparency) var reduceTransparency
-    
     @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    @Environment(\.scenePhase) var scenePhase
     
+    @State private var isActive = true
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
                 ZStack {
-                    Image(.background)
-                        .resizable()
-                        .ignoresSafeArea()
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
                             withAnimation {
@@ -47,6 +57,20 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .padding()
                 }
+            }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
             }
         }
     }
