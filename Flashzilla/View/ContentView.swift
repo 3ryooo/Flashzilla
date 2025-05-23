46f11f1a8f628bafcfa4124ba41b686aefb44568
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-23 16:07:42 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/moving-views-with-draggesture-and-offset

--- Flashzilla/View/ContentView.swift
+++ Flashzilla/View/ContentView.swift
@@ -18,13 +18,22 @@ struct ContentView: View {
                         .resizable()
                         .ignoresSafeArea()
                     ForEach(0..<cards.count, id: \.self) { index in
-                        CardView(card: cards[index])
-                            .stacked(at: index, in: cards.count)
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
                     }
                 }
             }
         }
     }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+    }
+    
 }
 
 extension View {
