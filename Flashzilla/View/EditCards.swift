1b2df92a0d2e63c8ece0c991085da996e16b2319
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-24 19:40:50 +0900

    add: カード追加後にテキストフィールドをクリアにする

--- Flashzilla/View/EditCards.swift
+++ Flashzilla/View/EditCards.swift
@@ -56,6 +56,10 @@ struct EditCards: View {
         if let data = try? JSONEncoder().encode(cards) {
             UserDefaults.standard.set(data, forKey: "Cards")
         }
+        
+        newPrompt = ""
+        newAnswer = ""
+        
     }
     
     func addCard() {
